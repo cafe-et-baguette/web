1bf9bca9e6d9be5363d07944492fe1604627b54f
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2023-04-21 10:19:53 +0700

    get user name and email for messages

--- src/pages/chatroom/[roomId].tsx
+++ src/pages/chatroom/[roomId].tsx
@@ -1,4 +1,6 @@
+import { User, userFromId } from "@/utils/auth.services";
 import axios from "axios";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { Socket, io } from "socket.io-client";
 
@@ -8,17 +10,24 @@ const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL
 
 let socket: Socket;
 export type Message = {
-  userId: string;
+  user: User;
   roomId: string;
   content: string;
   createdDate?: Date;
 };
 
 const ChatRoom = () => {
-  const [roomId, setRoomId] = useState<string>("");
-  const [userId, setUserId] = useState<string>("");
-  const [message, setMessage] = useState<string>("");
-  const [recvMsg, setRecvMsg] = useState<Message[]>([]);
+  // Details to send
+  const router = useRouter();
+  const roomId =
+    typeof router.query.roomId === "string" ? router.query.roomId : "";
+
+  const [userId, setUserId] = useState<string>(""); // TODO: Infer user from session
+  const [content, setContent] = useState<string>("");
+
+  // Messages
+  const [messages, setMessages] = useState<Message[]>([]);
+
   const connectSocket = () => {
     socket = io(BACKEND_URL);
     socket.on("connect", () => console.log(`Connected to ${BACKEND_URL}`));
@@ -28,38 +37,56 @@ const ChatRoom = () => {
 
   useEffect(() => {
     if (roomId) {
-      axios.get(`${BACKEND_URL}/chatroom/${roomId}/messages`).then((res) => {
-        const previousMessages = res.data;
-        setRecvMsg(
-          previousMessages.map((previousMessage: any) => {
-            previousMessage.createdDate = new Date(previousMessage.createdDate);
-            return previousMessage as Message;
-          })
-        );
-      });
+      // Get previous messages
+      axios
+        .get(`${BACKEND_URL}/chatroom/${roomId}/messages`)
+        .then(({ data }) => {
+          const previousMessages = data.map((messageResponse: any) => {
+            return userFromId(messageResponse.userId).then((user) => {
+              return {
+                user: user as User,
+                roomId: messageResponse.roomId,
+                content: messageResponse.content,
+                createdDate: new Date(messageResponse.createdDate),
+              } as Message;
+            });
+          });
+          Promise.all(previousMessages).then((previousMessages) => {
+            setMessages(previousMessages);
+          });
+        });
 
       while (!socket) {
         connectSocket();
-      }
+      } // TODO: Refactor this?
 
-      socket.on("broadcast", (msg: any) => {
-        msg.createdDate = new Date(msg.createdDate);
-        if (msg.roomId === roomId) {
-          setRecvMsg(recvMsg.concat(msg as Message));
+      // On broadcast add message to message list
+      socket.on("broadcast", (receivedMessage: any) => {
+        if (receivedMessage.roomId === roomId) {
+          setMessages(
+            messages.concat({
+              user: receivedMessage.user as User,
+              roomId: receivedMessage.roomId,
+              content: receivedMessage.content,
+              createdDate: new Date(receivedMessage.createdDate),
+            } as Message)
+          );
         }
       });
     }
-  }, [recvMsg, roomId]);
+  }, [messages, roomId]);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const messageToSend: Message = {
+    const messageToSend = {
       roomId: roomId,
       userId: userId,
-      content: message,
+      content: content,
     };
+    setContent("");
     socket.emit("send", messageToSend);
   };
+
   return (
     <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
       <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
@@ -67,26 +94,20 @@ const ChatRoom = () => {
           <h1 className=" text-gray-900 text-2xl font-semibold">Chat Room</h1>
         </div>
         <div>
-          {recvMsg.map((msg, idx) => {
+          {messages.map((message, idx) => {
             return (
               <div key={idx}>
-                <div>From: {msg.userId}</div>
-                <div>Message: {msg.content}</div>
-                <div>Time: {msg.createdDate?.toISOString()}</div>
+                <div>
+                  From: {message.user.name} &lt;{message.user.email}&gt;
+                </div>
+                <div>Message: {message.content}</div>
+                <div>Time: {message.createdDate?.toISOString()}</div>
                 <div>----</div>
               </div>
             );
           })}
         </div>
         <form onSubmit={handleSubmit}>
-          <input
-            id="roomId"
-            name="roomId"
-            placeholder="Room ID"
-            className="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
-            value={roomId}
-            onChange={(e) => setRoomId(e.target.value)}
-          ></input>
           <input
             id="userId"
             name="userId"
@@ -100,9 +121,9 @@ const ChatRoom = () => {
             name="message"
             placeholder="Message to send"
             className="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
-            value={message}
+            value={content}
             onChange={(e) => {
-              setMessage(e.target.value);
+              setContent(e.target.value);
             }}
           ></input>
           <button className="bg-gradient-to-r from-sky-300 to-rose-300 text-gray-900 rounded-md px-2 py-1">
