6a83bee0c4a972a1a2a574be919db71da13b9569
Passamon <Proud_passamon@hotmail.co.th>
2023-04-21 17:58:36 +0700

    style chat page

--- src/pages/chatroom/[roomId].tsx
+++ src/pages/chatroom/[roomId].tsx
@@ -93,53 +93,55 @@ const ChatRoom = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
-      <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
-        {/* <div className="absolute inset-0 bg-gradient-to-r from-sky-300 to-rose-300 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-2xl"></div> */}
-        <div className="text-center">
-          <h1 className=" text-gray-900 text-2xl font-semibold">Chat Room</h1>
-          <h1 className=" text-gray-900 text-sm font-semibold">Room ID</h1>
-        </div>
-        <div className="flex flex-row">
-          <div>
-            <div className="relative bg-gray-100 shadow-lg sm:rounded-2xl sm:p-20 space-y-3">
-              {messages.map((message, idx) => {
-                return (
-                  <div key={idx} className="px-4 py-2 bg-gray-300 rounded-xl ">
-                    <div className=" text-gray-900 text-sm font-semibold">
-                      From: {message.user.name} &lt;
-                      {message.user.email}&gt;
-                    </div>
-                    <div className=" text-gray-900 text-sm font-semibold">
-                      Message: {message.content}
-                    </div>
-                    <div className=" text-gray-900 text-sm font-semibold">
-                      Time: {message.createdDate?.toISOString()}
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
-            <div className="relative bg-gray-100 shadow-lg sm:rounded-2xl sm:p-20">
-              <form onSubmit={handleSubmit}>
-                <input
-                  id="message"
-                  name="message"
-                  placeholder="Message to send"
-                  className="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
-                  value={content}
-                  onChange={(e) => {
-                    setContent(e.target.value);
-                  }}
-                ></input>
-                <button className="bg-gradient-to-r from-sky-300 to-rose-300 text-gray-900 rounded-md px-2 py-1">
-                  Send
-                </button>
-              </form>
-            </div>
+    <div className="min-h-screen bg-gray-100 py-3 flex flex-col justify-center">
+      {/* <div className="absolute inset-0 bg-gradient-to-r from-sky-300 to-rose-300 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-2xl"></div> */}
+      <div className="relative px-4 bg-white shadow-lg sm:rounded-3xl sm:p-10 ">
+        <div className="flex flex-col w-auto border-4 border-yellow-500">
+          <div className="text-center">
+            <h1 className=" text-gray-900 text-2xl font-semibold">Chat Room</h1>
+            <h1 className=" text-gray-900 text-sm font-semibold">Room ID</h1>
           </div>
-          <div className="relative bg-gray-100 sm:rounded-2xl sm:p-20 ml-1">
-            <h1> ABC </h1>
+          <div className="flex flex-row w-auto border-4 border-red-500">
+            <div className="w-full h-full border-4 border-green-500">
+              <div className="bg-gray-100 shadow-lg sm:rounded-2xl sm:px-5 py-3 space-y-3 overflow-y-auto h-96 mt-1">
+                {messages.map((message, idx) => {
+                  return (
+                    <div key={idx} className="px-4 py-2 bg-gray-300 rounded-xl">
+                      <div className=" text-gray-900 text-sm font-semibold">
+                        From: {message.user.name} &lt;
+                        {message.user.email}&gt;
+                      </div>
+                      <div className=" text-gray-900 text-sm font-semibold">
+                        Message: {message.content}
+                      </div>
+                      <div className=" text-gray-900 text-sm font-semibold">
+                        Time: {message.createdDate?.toISOString()}
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+              <div className="relative bg-gray-100 shadow-lg sm:rounded-2xl h-1/5 mt-2 p-3">
+                <form onSubmit={handleSubmit} className="flex flex-row">
+                  <input
+                    id="message"
+                    name="message"
+                    placeholder="Message to send"
+                    className="peer w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600 mr-10"
+                    value={content}
+                    onChange={(e) => {
+                      setContent(e.target.value);
+                    }}
+                  ></input>
+                  <button className="bg-gradient-to-r from-sky-300 to-rose-300 text-gray-900 rounded-md px-8 py-1 mr-5">
+                    Send
+                  </button>
+                </form>
+              </div>
+            </div>
+            <div className="relative bg-gray-100 sm:rounded-2xl sm:p-20 ml-1">
+              <h1> ABC </h1>
+            </div>
           </div>
         </div>
       </div>
