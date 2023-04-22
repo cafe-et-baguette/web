9e1a2d5d63b4a2a06096c0e31100a1f20f204461
Passamon <Proud_passamon@hotmail.co.th>
2023-04-22 14:29:46 +0700

    change message design & add back button

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -3,7 +3,14 @@ function Search() {
     <div className="min-h-screen bg-gradient-to-r from-sky-300 to-rose-300 py-3 flex flex-col justify-center">
       <div className="relative px-4 bg-white shadow-lg sm:rounded-3xl sm:p-10 ">
         <div className="text-center">
-          <h1 className=" text-gray-900 text-2xl font-semibold">Search</h1>
+          <div className="flex flex-row w-1/2">
+            <button className="bg-gradient-to-r from-sky-300 to-rose-300 text-gray-900 rounded-md px-10 py-1 ml-5 ">
+              Back
+            </button>
+            <h1 className=" text-gray-900 text-2xl font-semibold ml-auto">
+              Search
+            </h1>
+          </div>
         </div>
         <div className="flex flex-row w-auto ">
           <div className="w-full h-full ">
@@ -13,7 +20,7 @@ function Search() {
                 className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
                 placeholder="search"
               />
-              <button className="bg-gradient-to-r from-sky-300 to-rose-300 text-gray-900 rounded-md px-8 py-1 ml-5">
+              <button className="bg-gradient-to-r from-sky-300 to-rose-300 text-gray-900 rounded-md px-8 py-1 ml-5 ">
                 Search
               </button>
             </div>
@@ -23,13 +30,13 @@ function Search() {
                   <div className=" text-gray-700 text-sm font-semibold">
                     from
                   </div>
-                  <div className=" text-gray-400 text-sm font-semibold ml-3">
-                    time
-                  </div>
                 </div>
-                <div className="px-4 py-3 bg-blue-300 rounded-xl">
+                <div className="px-4 py-3 bg-blue-200  rounded-xl flex flex-row w-auto">
                   <div className=" text-gray-900 text-sm font-semibold">
                     message
+                  </div>
+                  <div className=" text-gray-400 text-sm font-semibold ml-auto">
+                    time
                   </div>
                 </div>
               </div>
