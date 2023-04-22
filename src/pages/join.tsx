4dfb9f8522daaea5393f6443a284700df2164dc3
Ryn Parin <ryn.parin@gmail.com>
2023-04-23 02:38:53 +0700

    add rename and logout

--- src/pages/join.tsx
+++ src/pages/join.tsx
@@ -1,62 +1,68 @@
-import { useRouter } from "next/router";
-import React, { SyntheticEvent } from "react";
+import { useAuth } from '@/context/AuthContext';
+import { useRouter } from 'next/router';
+import React, { SyntheticEvent } from 'react';
 
 function Join() {
-  const router = useRouter();
+	const router = useRouter();
+	const { auth, setAuth } = useAuth();
 
-  return (
-    <div className="flex flex-col ">
-      <div className="flex flex-row relative h-14 bg-gradient-to-r from-sky-300 to-rose-300 shadow-lg items-center ">
-        <h1 className="text-gray-900 text-2xl font-semibold p-2">Nickname</h1>
-        <button className=" text-gray-600 rounded-md ml-1 text-base">
-          Edit
-        </button>
-        <button className="bg-white text-gray-900 rounded-md px-8 py-1 text-xl ml-auto mr-3">
-          My Chats
-        </button>
-        <button className="bg-white text-gray-900 rounded-md px-8 py-1 text-xl mr-2">
-          Logout
-        </button>
-      </div>
-      <div className="flex flex-row w-auto mt-32 mb-32">
-        <div className="w-full h-full m-4">
-          <div className="bg-gray-200 sm:rounded-2xl sm:px-5 py-3 h-96 divide-y-2 divide-gray-400">
-            <div>
-              <div className="text-center">
-                <h1 className=" text-gray-900 text-2xl font-semibold">
-                  Groups
-                </h1>
-              </div>
-            </div>
-            <div className="h-80 overflow-y-auto">
-              <div className="flex flex-row items-center bg-white sm:px-5 py-3 space-y-3 m-1">
-                Group 1
-                <button className="bg-gradient-to-r from-sky-300 to-rose-300 text-gray-900 rounded-md px-8 py-1 ml-auto">
-                  Join
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="w-full h-full m-4">
-          <div className="bg-gray-200 sm:rounded-2xl sm:px-5 py-3 h-96 divide-y-2 divide-gray-400">
-            <div>
-              <div className="text-center">
-                <h1 className=" text-gray-900 text-2xl font-semibold">Chats</h1>
-              </div>
-            </div>
-            <div className="h-80 overflow-y-auto">
-              <div className="flex flex-row items-center bg-white sm:px-5 py-3 space-y-3 m-1">
-                Chat 1
-                <button className="bg-gradient-to-r from-sky-300 to-rose-300 text-gray-900 rounded-md px-8 py-1 ml-auto">
-                  Join
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex flex-col ">
+			<div className="flex flex-row relative h-14 bg-gradient-to-r from-sky-300 to-rose-300 shadow-lg items-center ">
+				<h1 className="text-gray-900 text-2xl font-semibold p-2">
+					{auth?.name}
+				</h1>
+				<button className=" text-gray-600 rounded-md ml-1 text-base">
+					Edit
+				</button>
+				<button className="bg-white text-gray-900 rounded-md px-8 py-1 text-xl ml-auto mr-3">
+					My Chats
+				</button>
+				<button className="bg-white text-gray-900 rounded-md px-8 py-1 text-xl mr-2">
+					Logout
+				</button>
+			</div>
+			<div className="flex flex-row w-auto mt-32 mb-32">
+				<div className="w-full h-full m-4">
+					<div className="bg-gray-200 sm:rounded-2xl sm:px-5 py-3 h-96 divide-y-2 divide-gray-400">
+						<div>
+							<div className="text-center">
+								<h1 className=" text-gray-900 text-2xl font-semibold">
+									Groups
+								</h1>
+							</div>
+						</div>
+						<div className="h-80 overflow-y-auto">
+							<div className="flex flex-row items-center bg-white sm:px-5 py-3 space-y-3 m-1">
+								Group 1
+								<button className="bg-gradient-to-r from-sky-300 to-rose-300 text-gray-900 rounded-md px-8 py-1 ml-auto">
+									Join
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="w-full h-full m-4">
+					<div className="bg-gray-200 sm:rounded-2xl sm:px-5 py-3 h-96 divide-y-2 divide-gray-400">
+						<div>
+							<div className="text-center">
+								<h1 className=" text-gray-900 text-2xl font-semibold">
+									Chats
+								</h1>
+							</div>
+						</div>
+						<div className="h-80 overflow-y-auto">
+							<div className="flex flex-row items-center bg-white sm:px-5 py-3 space-y-3 m-1">
+								Chat 1
+								<button className="bg-gradient-to-r from-sky-300 to-rose-300 text-gray-900 rounded-md px-8 py-1 ml-auto">
+									Join
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 export default Join;
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,98 +1,102 @@
-import { useRouter } from "next/router";
-import React, { SyntheticEvent } from "react";
-import axios from "./api/axios";
+import { useRouter } from 'next/router';
+import React, { SyntheticEvent } from 'react';
+import axios from './api/axios';
 function Login() {
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
+	const [email, setEmail] = React.useState('');
+	const [password, setPassword] = React.useState('');
 
-  const router = useRouter();
+	const router = useRouter();
 
-  const handleSubmit = async (e: SyntheticEvent) => {
-    e.preventDefault();
-    try {
-      const res = await axios.post("/auth/login", { email, password });
-      console.log(res);
+	const handleSubmit = async (e: SyntheticEvent) => {
+		e.preventDefault();
+		try {
+			const res = await axios.post('/auth/login', { email, password });
+			console.log(res);
 
-      router.push("/");
-    } catch (error) {
-      console.log(error);
-    }
-  };
+			router.push('/');
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  return (
-    <div className="flex flex-col">
-      <div className="flex flex-row relative h-14 bg-gradient-to-r from-sky-300 to-rose-300 shadow-lg items-center ">
-        <button className="bg-white text-gray-900 rounded-md px-11 py-1 ml-auto text-xl mr-2">
-          Login
-        </button>
-        <button className="bg-white text-gray-900 rounded-md px-8 py-1 text-xl mr-2">
-          Register
-        </button>
-      </div>
-      <div className="relative py-3 sm:max-w-xl sm:mx-auto mt-36">
-        <div className="absolute inset-0 bg-gradient-to-r from-sky-300 to-rose-300 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
-        <form
-          onSubmit={handleSubmit}
-          className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20"
-        >
-          <div className="max-w-md mx-auto">
-            <div>
-              <h1 className=" text-gray-900 text-2xl font-semibold">
-                Login Form
-              </h1>
-            </div>
-            <div className="divide-y divide-gray-200">
-              <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
-                <div className="relative">
-                  <input
-                    autoComplete="off"
-                    id="email"
-                    name="email"
-                    required
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    type="text"
-                    className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
-                    placeholder="Email address"
-                  />
-                  <label
-                    htmlFor="email"
-                    className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
-                  >
-                    Email Address
-                  </label>
-                </div>
-                <div className="relative">
-                  <input
-                    autoComplete="off"
-                    id="password"
-                    name="password"
-                    type="password"
-                    required
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
-                    placeholder="Password"
-                  />
-                  <label
-                    htmlFor="password"
-                    className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
-                  >
-                    Password
-                  </label>
-                </div>
-                <div className="relative">
-                  <button className="bg-gradient-to-r from-sky-300 to-rose-300 text-gray-900 rounded-md px-2 py-1">
-                    Submit
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+	return (
+		<div className="flex flex-col">
+			<div className="flex flex-row relative h-14 bg-gradient-to-r from-sky-300 to-rose-300 shadow-lg items-center ">
+				<button className="bg-white text-gray-900 rounded-md px-11 py-1 ml-auto text-xl mr-2">
+					Login
+				</button>
+				<button className="bg-white text-gray-900 rounded-md px-8 py-1 text-xl mr-2">
+					Register
+				</button>
+			</div>
+			<div className="relative py-3 sm:max-w-xl sm:mx-auto mt-36">
+				<div className="absolute inset-0 bg-gradient-to-r from-sky-300 to-rose-300 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+				<form
+					onSubmit={handleSubmit}
+					className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20"
+				>
+					<div className="max-w-md mx-auto">
+						<div>
+							<h1 className=" text-gray-900 text-2xl font-semibold">
+								Login Form
+							</h1>
+						</div>
+						<div className="divide-y divide-gray-200">
+							<div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+								<div className="relative">
+									<input
+										autoComplete="off"
+										id="email"
+										name="email"
+										required
+										value={email}
+										onChange={(e) =>
+											setEmail(e.target.value)
+										}
+										type="text"
+										className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
+										placeholder="Email address"
+									/>
+									<label
+										htmlFor="email"
+										className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+									>
+										Email Address
+									</label>
+								</div>
+								<div className="relative">
+									<input
+										autoComplete="off"
+										id="password"
+										name="password"
+										type="password"
+										required
+										value={password}
+										onChange={(e) =>
+											setPassword(e.target.value)
+										}
+										className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
+										placeholder="Password"
+									/>
+									<label
+										htmlFor="password"
+										className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+									>
+										Password
+									</label>
+								</div>
+								<div className="relative">
+									<button className="bg-gradient-to-r from-sky-300 to-rose-300 text-gray-900 rounded-md px-2 py-1">
+										Submit
+									</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 }
 
 export default Login;
